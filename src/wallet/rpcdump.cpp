3a06ac50842d8c4174758a2eb9010c4eeb7e8f1c
Jon Spock <jonspock@protonmail.com>
2018-03-10 16:05:26 -0800

    Name changes bitcoin -> maza

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -76,11 +76,11 @@ UniValue importprivkey(const Config &config, const JSONRPCRequest &request) {
 
     if (request.fHelp || request.params.size() < 1 || request.params.size() > 3)
         throw std::runtime_error(
-            "importprivkey \"bitcoinprivkey\" ( \"label\" ) ( rescan )\n"
+            "importprivkey \"mazaprivkey\" ( \"label\" ) ( rescan )\n"
             "\nAdds a private key (as returned by dumpprivkey) to your "
             "wallet.\n"
             "\nArguments:\n"
-            "1. \"bitcoinprivkey\"   (string, required) The private key (see "
+            "1. \"mazaprivkey\"   (string, required) The private key (see "
             "dumpprivkey)\n"
             "2. \"label\"            (string, optional, default=\"\") An "
             "optional label\n"
@@ -253,7 +253,7 @@ UniValue importaddress(const Config &config, const JSONRPCRequest &request) {
         ImportScript(CScript(data.begin(), data.end()), strLabel, fP2SH);
     } else {
         throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY,
-                           "Invalid Bitcoin address or script");
+                           "Invalid Maza address or script");
     }
 
     if (fRescan) {
@@ -556,7 +556,7 @@ UniValue dumpprivkey(const Config &config, const JSONRPCRequest &request) {
             "\nReveals the private key corresponding to 'address'.\n"
             "Then the importprivkey can be used with this output\n"
             "\nArguments:\n"
-            "1. \"address\"   (string, required) The bitcoin address for the "
+            "1. \"address\"   (string, required) The maza address for the "
             "private key\n"
             "\nResult:\n"
             "\"key\"                (string) The private key\n"
@@ -573,7 +573,7 @@ UniValue dumpprivkey(const Config &config, const JSONRPCRequest &request) {
     CBitcoinAddress address;
     if (!address.SetString(strAddress))
         throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY,
-                           "Invalid Bitcoin address");
+                           "Invalid Maza address");
     CKeyID keyID;
     if (!address.GetKeyID(keyID))
         throw JSONRPCError(RPC_TYPE_ERROR, "Address does not refer to a key");
@@ -624,7 +624,7 @@ UniValue dumpwallet(const Config &config, const JSONRPCRequest &request) {
     std::sort(vKeyBirth.begin(), vKeyBirth.end());
 
     // produce output
-    file << strprintf("# Wallet dump created by Bitcoin %s\n", CLIENT_BUILD);
+    file << strprintf("# Wallet dump created by Maza %s\n", CLIENT_BUILD);
     file << strprintf("# * Created on %s\n", EncodeDumpTime(GetTime()));
     file << strprintf("# * Best block at time of backup was %i (%s),\n",
                       chainActive.Height(),
