5ff0a523a2b681f3fc92754eb34806feec43c181
Jon Spock <jonspock@protonmail.com>
2018-03-31 11:50:57 -0700

    cleanup of mining

--- src/chain.cpp
+++ src/chain.cpp
@@ -4,6 +4,31 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "chain.h"
+#include "chainparams.h"
+#include "validation.h"
+
+/* Moved here from the header, because we need auxpow and the logic becomes more involved.  */
+CBlockHeader CBlockIndex::GetBlockHeader(const Consensus::Params& consensusParams) const
+{
+    CBlockHeader block;
+    block.nVersion       = nVersion;
+
+    /* The CBlockIndex object's block header is missing the auxpow.
+       So if this is an auxpow block, read it from disk instead.  We only have to read the actual *header*, not the full block.  */
+    if (block.IsAuxpow())
+    {
+        ReadBlockHeaderFromDisk(block, this, consensusParams);
+        return block;
+    }
+
+    if (pprev)
+        block.hashPrevBlock = pprev->GetBlockHash();
+    block.hashMerkleRoot = hashMerkleRoot;
+    block.nTime          = nTime;
+    block.nBits          = nBits;
+    block.nNonce         = nNonce;
+    return block;
+}
 
 /**
  * CChain implementation
@@ -113,7 +138,8 @@ void CBlockIndex::BuildSkip() {
     if (pprev) pskip = pprev->GetAncestor(GetSkipHeight(nHeight));
 }
 
-arith_uint256 GetBlockProof(const CBlockIndex &block) {
+arith_uint256 GetBlockProofBase(const CBlockIndex& block)
+{
     arith_uint256 bnTarget;
     bool fNegative;
     bool fOverflow;
@@ -126,10 +152,161 @@ arith_uint256 GetBlockProof(const CBlockIndex &block) {
     return (~bnTarget / (bnTarget + 1)) + 1;
 }
 
-int64_t GetBlockProofEquivalentTime(const CBlockIndex &to,
-                                    const CBlockIndex &from,
-                                    const CBlockIndex &tip,
-                                    const Consensus::Params &params) {
+arith_uint256 GetPrevWorkForAlgo(const CBlockIndex& block, int algo)
+{
+    const CBlockIndex* pindex = &block;
+    while (pindex != NULL)
+    {
+        if (pindex->GetAlgo() == algo)
+        {
+            return GetBlockProofBase(*pindex);
+        }
+        pindex = pindex->pprev;
+    }
+    return UintToArith256(Params().GetConsensus().powLimit[algo]);
+}
+
+arith_uint256 GetPrevWorkForAlgoWithDecayV1(const CBlockIndex& block, int algo)
+{
+    int nDistance = 0;
+    const CBlockIndex* pindex = &block;
+    pindex = pindex->pprev;
+    arith_uint256 ret = UintToArith256(Params().GetConsensus().powLimit[algo]);
+    while (pindex != NULL)
+    {
+        if (nDistance > 32)
+        {
+            return ret;
+        }
+        if (pindex->GetAlgo() == algo)
+        {
+            arith_uint256 nWork = GetBlockProofBase(*pindex);
+            nWork *= (32 - nDistance);
+            nWork /= 32;
+            if (nWork < ret) nWork = ret;
+            return nWork;
+        }
+        pindex = pindex->pprev;
+        nDistance++;
+    }
+    return ret;
+}
+// same as V1 but default is 0
+arith_uint256 GetPrevWorkForAlgoWithDecayV2(const CBlockIndex& block, int algo)
+{
+    int nDistance = 0;
+    const CBlockIndex* pindex = &block;
+    pindex = pindex->pprev;
+    while (pindex != NULL)
+    {
+        if (nDistance > 32)
+        {
+            return arith_uint256(0);
+        }
+        if (pindex->GetAlgo() == algo)
+        {
+            arith_uint256 nWork = GetBlockProofBase(*pindex);
+            nWork *= (32 - nDistance);
+            nWork /= 32;
+            return nWork;
+        }
+        pindex = pindex->pprev;
+        nDistance++;
+    }
+    return arith_uint256(0);
+}
+// similar to V2 but using '100' instead of '32'
+arith_uint256 GetPrevWorkForAlgoWithDecayV3(const CBlockIndex& block, int algo)
+{
+    int nDistance = 0;
+    const CBlockIndex* pindex = &block;
+    pindex = pindex->pprev;
+    while (pindex != NULL)
+    {
+        if (nDistance > 100)
+        {
+            return arith_uint256(0);
+        }
+        if (pindex->GetAlgo() == algo)
+        {
+            arith_uint256 nWork = GetBlockProofBase(*pindex);
+            nWork *= (100 - nDistance);
+            nWork /= 100;
+            return nWork;
+        }
+        pindex = pindex->pprev;
+        nDistance++;
+    }
+    return arith_uint256(0);
+}
+
+arith_uint256 GetGeometricMeanPrevWork(const CBlockIndex& block)
+{
+    //arith_uint256 bnRes;
+    arith_uint256 nBlockWork = GetBlockProofBase(block);
+    uint256 bnBlockWork = ArithToUint256(nBlockWork);
+    int nAlgo = block.GetAlgo();
+    
+    for (int algo = 0; algo < NUM_ALGOS; algo++)
+    {
+        if (algo != nAlgo)
+        {
+            arith_uint256 nBlockWorkAlt = GetPrevWorkForAlgoWithDecayV3(block, algo);
+            uint256 bnBlockWorkAlt = ArithToUint256(nBlockWorkAlt);
+            if (nBlockWorkAlt != 0)
+                nBlockWork *= nBlockWorkAlt;
+        }
+    }
+    // Compute the geometric mean
+    /* XWARNING
+    CBigNum bnRes = bnBlockWork.nthRoot(NUM_ALGOS);
+    
+    // Scale to roughly match the old work calculation
+    bnRes <<= 8;
+    
+    //return bnRes;
+    return UintToArith256(bnRes.getuint256());
+     */
+    return nBlockWork;
+}
+
+arith_uint256 GetBlockProof(const CBlockIndex& block)
+{
+    Consensus::Params params = Params().GetConsensus();
+
+    arith_uint256 bnTarget;
+    int nHeight = block.nHeight;
+    int nAlgo = block.GetAlgo();
+
+    if (nHeight > params.nGeometricAverageWork_Start)
+    {
+        bnTarget = GetGeometricMeanPrevWork(block);
+    }
+    else
+    {
+        arith_uint256 nBlockWork = GetBlockProofBase(block);
+        for (int algo = 0; algo < NUM_ALGOS; algo++)
+        {
+            if (algo != nAlgo)
+            {
+                if(nHeight >= params.nBlockAlgoNormalisedWorkDecayV2Start)
+                {
+                    nBlockWork += GetPrevWorkForAlgoWithDecayV2(block, algo);
+                }
+                else
+                {
+                    nBlockWork += GetPrevWorkForAlgoWithDecayV1(block, algo);
+                }
+            }
+        }
+        bnTarget = nBlockWork / NUM_ALGOS;
+    }
+    
+    return bnTarget;
+}
+
+int64_t GetBlockProofEquivalentTime(const CBlockIndex& to, const CBlockIndex& from, const CBlockIndex& tip, const Consensus::Params& params)
+{
     arith_uint256 r;
     int sign = 1;
     if (to.nChainWork > from.nChainWork) {
@@ -143,4 +320,29 @@ int64_t GetBlockProofEquivalentTime(const CBlockIndex &to,
         return sign * std::numeric_limits<int64_t>::max();
     }
     return sign * r.GetLow64();
+}
+
+const CBlockIndex* GetLastBlockIndexForAlgo(const CBlockIndex* pindex, int algo)
+{
+    for (;;)
+    {
+        if (!pindex)
+            return NULL;
+        if (pindex->GetAlgo() == algo)
+            return pindex;
+        pindex = pindex->pprev;
+    }
+}
+
+std::string GetAlgoName(int Algo, uint32_t time, const Consensus::Params& consensusParams)
+{
+    switch (Algo)
+    {
+        case ALGO_SLOT1:  return std::string("Lyra2RE2");
+        case ALGO_SLOT2:  return std::string("Skein");
+        case ALGO_SLOT3:  return std::string("Argon2d");
+        case ALGO_SLOT4:  return std::string("SHA256");
+
+    }
+    return std::string("Unknown");
 }
---
+++ src/primitives/pureheader.cpp
@@ -0,0 +1,65 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "primitives/pureheader.h"
+
+#include "hash.h"
+#include "crypto/hashskein.h"
+#include "crypto/lyra2/lyra2RE.h"
+#include "crypto/hashargon2d.h"
+#include "utilstrencodings.h"
+
+uint256 CPureBlockHeader::GetHash() const
+{
+    return SerializeHash(*this);
+}
+
+uint256 CPureBlockHeader::GetPoWHash(int algo, const Consensus::Params& consensusParams) const
+{
+    switch (algo)
+    {
+        case ALGO_SLOT1:
+            {
+            uint256 thash;
+            lyra2re2_hash(BEGIN(nVersion), BEGIN(thash));
+            return thash;
+            }
+            
+        case ALGO_SLOT2:
+            return HashSkein(BEGIN(nVersion), END(nNonce));
+            
+        case ALGO_SLOT3:
+            return HashArgon2d(BEGIN(nVersion), END(nNonce));
+            
+        case ALGO_SLOT4:
+            return GetHash();
+            
+    }
+    // catch-all if above doesn't match anything to algo
+    return GetHash();
+}
+
+void CPureBlockHeader::SetBaseVersion(int32_t nBaseVersion, int32_t nChainId)
+{
+    assert(nBaseVersion >= 1 && nBaseVersion < VERSION_AUXPOW);
+    assert(!IsAuxpow());
+    nVersion = nBaseVersion | (nChainId * VERSION_CHAIN_START);
+}
+
+int GetAlgo(int nVersion)
+{
+    switch (nVersion & BLOCK_VERSION_ALGO)
+    {
+        case 0:
+            return ALGO_SLOT1;
+        case BLOCK_VERSION_SLOT2:
+            return ALGO_SLOT2;
+        case BLOCK_VERSION_SLOT3:
+            return ALGO_SLOT3;
+        case BLOCK_VERSION_SLOT4:
+            return ALGO_SLOT4;
+    }
+    return ALGO_SLOT1;
+}
